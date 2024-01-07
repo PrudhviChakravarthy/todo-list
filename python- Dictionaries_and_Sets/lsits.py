6ce05363ad4a3a02589b16d3eb6c3fe63dac2bf0
PrudhviChakravarthy <Prudhviideas@gmail.com>
2024-01-07 10:58:59 +0530

    lsits.py

---
+++ python- Dictionaries_and_Sets/lsits.py
@@ -0,0 +1,16 @@
+# Create a list
+fruits = ['apple', 'banana', 'cherry']
+
+# Append an element to the list
+fruits.append('orange')
+
+# Remove an element from the list
+fruits.remove('banana')
+
+# Accessing elements using indexing
+print('First fruit:', fruits[0])
+print('Last fruit:', fruits[-1])
+
+# Iterate over the list
+for fruit in fruits:
+    print(fruit)
