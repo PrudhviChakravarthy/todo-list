aa1332a2adae4198b09816bb3af739cdb1605553
PrudhviChakravarthy <Prudhviideas@gmail.com>
2024-01-07 15:25:31 +0530

    list_slice.py

---
+++ python- Dictionaries_and_Sets/list_slice.py
@@ -0,0 +1,7 @@
+# Example list
+fruits = ["apple", "banana", "cherry", "date", "fig"]
+
+# Extract elements from index 1 (inclusive) to 4 (exclusive)
+selected_fruits = fruits[1:4]
+
+print(selected_fruits)  # Output: ["banana", "cherry", "date"]
