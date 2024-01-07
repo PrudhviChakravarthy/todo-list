4f3fe01bcae4dbbef9b0785d54534843742166ee
PrudhviChakravarthy <Prudhviideas@gmail.com>
2024-01-07 13:45:40 +0530

    dicts.py

---
+++ python- Dictionaries_and_Sets/dicts.py
@@ -0,0 +1,11 @@
+# Create a dictionary
+student = {
+    'name': 'Alice',
+    'age': 25,
+    'grade': 'A'
+}
+
+# Accessing dictionary values
+print("Name:", student['name'])
+print("Age:", student['age'])
+print("Grade:", student['grade'])
