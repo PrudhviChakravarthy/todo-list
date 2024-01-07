4ccad941830ec67f0a04f10f6944aece34db345c
PrudhviChakravarthy <Prudhviideas@gmail.com>
2024-01-07 13:22:48 +0530

    set_with_duplicates.py

---
+++ python- Dictionaries_and_Sets/set_with_duplicates.py
@@ -0,0 +1,6 @@
+# Remove duplicates from a list using a set
+numbers = [1, 2, 2, 3, 4, 4, 5]
+unique_numbers = list(set(numbers))
+
+print('Original List:', numbers)
+print('List with Duplicates Removed:', unique_numbers)
