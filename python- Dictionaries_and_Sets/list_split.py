d57620ecc357f96131f43b9eb53ab3dd497e6681
PrudhviChakravarthy <Prudhviideas@gmail.com>
2024-01-07 16:28:09 +0530

    list_split.py

---
+++ python- Dictionaries_and_Sets/list_split.py
@@ -0,0 +1,13 @@
+# Example string
+sentence = "This is a sample sentence."
+
+# Split the string by spaces (default delimiter)
+words = sentence.split()
+
+print(words)  # Output: ["This", "is", "a", "sample", "sentence."]
+
+# Split the string by a custom delimiter (e.g., comma)
+csv_data = "Alice,Bob,Charlie,David"
+names = csv_data.split(",")
+
+print(names)  # Output: ["Alice", "Bob", "Charlie", "David"]
